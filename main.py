368e7b1cf6c3f60ad6a5c504940079d767111254
Randall Lowe <rwlowe@gmail.com>
2024-11-06 01:43:58 -0600

    Added triangle and collision circle

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -13,16 +14,15 @@ def main():
     #setting delta time
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+        player.draw(screen)
         pygame.display.flip()
-        
-        
-        
         
         dt = clock.tick(60)/1000
 
